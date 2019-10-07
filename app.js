44662380e917d41f086b0c2367e5216e5aead365
mmataja <mmataja92@gmail.com>
2019-10-07 14:59:06 +0200

    add cors middleware, create post method for register device

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.post('/register', (req, res) => {
+  console.log(req.body);
+  res.status(200).send(req.body);
 });
 
 app.listen(8000, () => {
