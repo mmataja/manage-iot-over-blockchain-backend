beb9a12b55e7a45646d10f4fa01f4fa98db135f0
mmataja <mmataja92@gmail.com>
2019-10-16 23:55:42 +0200

    add manageDevice contract and enable it's creation on register route

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const express = require('express')
 const bodyParser = require('body-parser');
 
 const web3 = new Web3("http://127.0.0.1:7545");
-const sourceCode = fs.readFileSync('./contracts/Add.sol', 'UTF-8');
+const sourceCode = fs.readFileSync('./contracts/ManageDevice.sol', 'UTF-8');
 
 const input = {
   language: 'Solidity',
   sources: {
-    'test.sol': {
+    'manageDevice.sol': {
       content: sourceCode
     }
   },
@@ -24,8 +24,8 @@ const input = {
 };
 
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
-const contractABI = output.contracts['test.sol'].Add.abi;
-const byteCode = output.contracts['test.sol'].Add.evm.bytecode.object;
+const contractABI = output.contracts['manageDevice.sol'].ManageDevice.abi;
+const byteCode = output.contracts['manageDevice.sol'].ManageDevice.evm.bytecode.object;
 const contractAddress = "0x0E35aFeE8bf0ebC3f2F6b99A0cCb623e68A33EAc";
 const addContract = new web3.eth.Contract(contractABI);
 
@@ -73,11 +73,18 @@ app.get('/call', async (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
-  const sum = contractInstance.methods.getSum().call();
-  console.log(req.body);
-  console.log("ovo je suma....", sum);
-  res.status(200).send(req.body);
+  const { account } = req.body;
+
+  addContract.deploy({data: byteCode}).send({
+    from: account,
+    gas: 1500000,
+  }).then(contractInstance => {
+    const contractAddress = contractInstance.options.address;
+    addContract.options.address = contractInstance.options.address;
+    
+    res.status(200).send(contractAddress);
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(8000, () => {
