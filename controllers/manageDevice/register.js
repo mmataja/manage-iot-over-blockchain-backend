9c7455f68ad9bba54fd3b14003f295260725c35b
mmataja <mmataja92@gmail.com>
2019-10-25 18:40:53 +0200

    get transaction data after device register

--- controllers/manageDevice/register.js
+++ controllers/manageDevice/register.js
@@ -4,12 +4,25 @@ const { createContract } = manageDevice;
 const { addContract, byteCode } = createContract;
 
 module.exports = async (req, res) => {
-  const { account } = req.body;
+  const { account, signature } = req.body;
 
-  const deviceRegisterContract = await addContract.deploy({data: byteCode}).send({
+  console.log("REQUEST", req.body);
+
+  const deviceRegisterContract = await addContract.deploy({ data: byteCode }).send({
     from: account,
     gas: 1500000,
   });
 
-  res.status(200).send(deviceRegisterContract.options.address);
+  await deviceRegisterContract.methods.registerDevice(signature).send({
+    from: account,
+    gas: 1500000,
+  }).on('receipt', (receipt) => {
+    console.log("OVO JE RAČUN", receipt);
+  });
+
+  // deviceRegisterContract.events.allEvents({}, (error, event) => console.log(event)).on('data', event => {
+  //   console.log("je ima tu nesto", event)
+  // });
+
+  res.status(200).send(deviceRegisterContract._address);
 }
