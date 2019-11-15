e3a6a00132e783a81426804759cb46a065998295
mmataja <mmataja92@gmail.com>
2019-11-15 20:11:21 +0100

    Create index file for models

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const contracts = require('./contracts');
+const devices = require('./devices');
+const users = require('./users');
+
+module.exports.Contracts = contracts;
+module.exports.Devices = devices;
+module.exports.Users = users;
