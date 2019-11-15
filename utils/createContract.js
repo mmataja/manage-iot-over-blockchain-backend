25f5d1d2a9523ced8e5a018fb42cd8385057ff9a
mmataja <mmataja92@gmail.com>
2019-11-15 20:12:13 +0100

    Rename addContract -> contractJSON

--- utils/createContract.js
+++ utils/createContract.js
@@ -24,9 +24,9 @@ const input = {
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
 const contractABI = output.contracts['manageDevice.sol'].ManageDevice.abi;
 const byteCode = output.contracts['manageDevice.sol'].ManageDevice.evm.bytecode.object;
-const addContract = new web3.eth.Contract(contractABI);
+const contractJSON = new web3.eth.Contract(contractABI);
 
 module.exports = { 
   byteCode, 
-  addContract
+  contractJSON
 }
