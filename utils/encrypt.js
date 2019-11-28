36840aabe5e6f2f2b7b72b0ae7ef48895f078180
mmataja <mmataja92@gmail.com>
2019-11-28 22:22:21 +0100

    Fix encrypt function

--- utils/encrypt.js
+++ utils/encrypt.js
@@ -1,15 +1,10 @@
 const crypto = require('crypto');
 const sshKeyToPEM = require('ssh-key-to-pem');
 
-module.exports = (data, blockNumber, publicKey) => {
-  const dataToEncrypt = {
-    data,
-    blockNumber
-  };
-
+module.exports = (data, publicKey) => {
   const sshPublicKey = sshKeyToPEM(publicKey);
 
-  const buffer = Buffer.from((dataToEncrypt, 'utf-8'));
+  const buffer = Buffer.from((data, 'utf-8'));
   const encryptedData = crypto.publicEncrypt(sshPublicKey, buffer);
   return encryptedData.toString('base64');
 }
