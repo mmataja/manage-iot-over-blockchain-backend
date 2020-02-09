4c5bfcd1063d8adeba039a97c53cda48ade0f5d7
mmataja <mmataja92@gmail.com>
2020-02-09 19:28:02 +0100

    Inital update device structure

--- controllers/manageDevice/update.js
+++ controllers/manageDevice/update.js
@@ -1,7 +1,19 @@
 const axios = require('axios');
 
+const { manageDevice } = require('../../services'); 
+
 module.exports = async (req, res) => {
-  const { id } = req.params;
+  const { id, account } = req.params;
+
+  if (!id || !account) {
+    res.status(400).json({ message: 'Invalid Id provided.' }).end();
+  }
+
+  const isContractOwner = await manageDevice.update(id, account);
+
+  if (!isContractOwner) {
+    res.status(400).json({ message: 'You are not owner of this device.' }).end();
+  }
 
   console.log("ID", id);
 
